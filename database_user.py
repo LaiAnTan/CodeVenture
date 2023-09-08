b9b4fb1956dd8aca4b9aa70a845c22b0011b7d87
laian <tanlaian2004@gmail.com>
2023-09-08 14:29:23 +0800

    feat: database_user is now singleton

--- database_user.py
+++ database_user.py
@@ -1,38 +1,72 @@
 import sqlite3 as sqlite3
 import os
 class UserDB:
-	
-	def __init__(self):
-		# open connection & init cursor
-		BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-		db_path = os.path.join(BASE_DIR, "users.db")
-		self.conn = sqlite3.connect(db_path)
-		self.cursor = self.conn.cursor()
-		print("Connected")
 
-	def exists(self):
-		self.cursor.execute("""SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='users'""")
-		return self.cursor.fetchone() != None
+    """
+    Singleton class that handles users database operations
+    """
+    # database path
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    db_path = os.path.join(BASE_DIR, "users.db")
+    
+    # instance
+    _instance = None
+    
+    # specify the fields below
+    db_fields = """
+                username text, 
+                password text, 
+                name text
+                """
+    
+    # placeholder for field
+    db_placeholders = "(" + "".join(["?, " for i in range(len(db_fields.split(",")) - 1)]) + "?" + ")"
+    
+    # connection and cursor
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    
+    class UserExistsException(Exception):
+        """Called when adding user that already exists in the database"""
+        
+        def __init__(self, msg="User already exists in the database"):
+            super().__init__(msg)
 
-	def new_users_db(self):
-		# ONLY RUN THIS FUNCTION IF YOU WANT A NEW USERS DATABASE TO BE CREATED
-		# specify the fields below
-		if self.exists() == False:
-			self.cursor.execute("CREATE TABLE users( username text, password text, name text)")
-			self.conn.commit()
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+        return cls._instance
 
-	def add_user(self, user_data: tuple):
-		# used placeholder (?) instead of named fields for easy addition of new fields in the future
-		self.cursor.execute("INSERT INTO users VALUES (?, ?, ?)", user_data)
-		self.conn.commit()
-	
-	def remove_user(self, username):
-		self.cursor.execute("DELETE FROM users WHERE username=:username", {"username": username})
-		self.conn.commit()
-	
-	def fetch_attr(self, field, username):
-		# fetches the required attribute with the username that matches it
-		return self.cursor.execute(f"SELECT {field} from users WHERE username=:username", {"username": username}).fetchone()
+    @classmethod
+    def db_exists(cls):
+        cls.cursor.execute("""SELECT tbl_name FROM sqlite_master WHERE type='table' AND tbl_name='users'""")
+        return cls.cursor.fetchone() != None
+
+    @classmethod
+    def new_users_db(cls):
+        # ONLY RUN THIS FUNCTION IF YOU WANT A NEW USERS DATABASE TO BE CREATED
+        if cls.db_exists() == False:
+            cls.cursor.execute(f"CREATE TABLE users({cls.db_fields})")
+            cls.conn.commit()
+
+    @classmethod
+    def add_user(cls, user_data: tuple):
+        # used placeholder (?) instead of named fields for easy addition of new fields in the future
+        if cls.fetch_attr("username", user_data[0]) != None:
+            raise cls.UserExistsException
+        cls.cursor.execute(f"INSERT INTO users VALUES {cls.db_placeholders}", user_data)
+        cls.conn.commit()
+
+    @classmethod
+    def remove_user(cls, username):
+        cls.cursor.execute("DELETE FROM users WHERE username=:username", {"username": username})
+        cls.conn.commit()
+
+    @classmethod
+    def fetch_attr(cls, field, username):
+        # fetches the required attribute with the username that matches it
+        return cls.cursor.execute(f"SELECT {field} from users WHERE username=:username", {"username": username}).fetchone()
 
 if __name__ == "__main__":
-	pass
+    pass
--- main.py
+++ main.py
@@ -3,18 +3,19 @@ from database_user import UserDB
 import os
 
 if __name__ == "__main__":
-    db = UserDB()
+    db = UserDB().instance()
     db.new_users_db()
-    if db.exists() == False:
+    if db.db_exists() == False:
         print("where did my db go")
     else:
         print("db exists")
     users = import_users_from_csv("test_users.txt")
     for user in users:
-        db.add_user(user)
+        try:
+            db.add_user(user)
+        except db.UserExistsException:
+            continue
     
     test = User(users[0][2], users[0][0], users[0][1])
     user_input_pw = input("Enter password: ")
     print(test.login(user_input_pw))
-
-# fucking
