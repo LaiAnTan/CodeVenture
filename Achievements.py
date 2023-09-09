eab22a0fbde86455db6c7dc8d40d5be784e1a918
aidanlah <aidanleongsw@gmail.com>
2023-09-09 22:28:05 +0800

    aaaaaaaaaaaaaaaaaaaaaa

--- Achievements.py
+++ Achievements.py
@@ -2,38 +2,63 @@ class Achievements():
     """
     Class for managing achievements and their criteria.
     """
+    
+    # Class attributes for Bronze, Silver, and Gold badges
+    bronze_badge = {
+        'name': "Bronze Badge",
+        'details': "Complete 5 learning modules",
+        'criteria': 5
+    }
+
+    silver_badge = {
+        'name': "Silver Badge",
+        'details': "Complete 15 learning modules",
+        'criteria': 15
+    }
+
+    gold_badge = {
+        'name': "Gold Badge",
+        'details': "Complete 30 learning modules",
+        'criteria': 30
+    }
+
+    badges = [bronze_badge, silver_badge, gold_badge]
 
     def __init__(self):
         self.achievements = []
 
-    def add_achievement(self, achievement_name: str, achievement_details: str, depending_criteria) -> None:
+    def add_achievement(self, module_name: str, module_details: str, depending_criteria: int) -> None:
         """
         Adds achievements.
 
-        achievement_name: Name of achievement.
-        achievement_details: Details of achievement.
+        module_name: Name of achievement.
+        module_details: Details of achievement.
         depending_criteria: Required completed modules.
         """
         achievement = {
-            'name': achievement_name,
-            'details': achievement_details,
-            'depending_criteria': depending_criteria,
+            'name': module_name,
+            'details': module_details,
+            'criteria': depending_criteria,
             'passed': False
+            #'badge_image' : image?
         }
+
         self.achievements.append(achievement)
 
-    def check_achievements(self, current_completed) -> None:
+
+    def check_achievements(self, module_name: str, current_completed: int) -> None:
         """
         Checks if achievement criteria has been met and marks as passed.
 
         current_completed: The current activity level.
         """
-        for achievement in self.achievements:
+        for achievement in Achievements.badges:
             if not achievement['passed'] and self.met_criteria(achievement['depending_criteria'], current_completed):
                 achievement['passed'] = True
-                print(f"Congratulations! You received the {achievement['name']}")
+                print(f"Congratulations! You received the {achievement['name']} for the {module_name} module.")
 
-    def met_criteria(self, depending_criteria, current_completed) -> bool:
+
+    def met_criteria(self, depending_criteria: int, current_completed: int) -> bool:
         """
         Check if the criteria for an achievement has been met.
 
@@ -52,11 +77,6 @@ if __name__ == "__main__":
 
     achievements = Achievements()
     current_completed = 5
-
-    # Adds an achievement
-    achievements.add_achievement("Bronze Badge", "Complete 5 learning modules", 5)
-    achievements.add_achievement("Silver Badge", "Complete 15 learning modules", 15)
-    achievements.add_achievement("Gold Badge", "Complete 30 learning modules", 30)
 
     # Checks if the achievement's criteria has been met
     achievements.check_achievements(current_completed)
