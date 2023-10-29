e9917b5c17cf3cf2ff48aa796a29626d0f876bff
NotChuaSai <chuashixiang@gmail.com>
2023-10-30 00:47:19 +0800

    focuses and scrolls the frame to the correct position after shifting

--- src/frontend/edu_windows/helper_class/entryShifter.py
+++ src/frontend/edu_windows/helper_class/entryShifter.py
@@ -40,38 +40,15 @@ class EntryShifterConfig(ctk.CTkFrame):
         if index == 0:
             return
         self.parent.swap_order(index, index - 1)
+        self.parent.scroll_frame((index - 1) / self.parent.get_tracking_no())
         self.parent.refresh_elements()
+        self.master.focus()
 
     def swap_down(self):
         index = self.master.get_index_instance()
         if index == (self.parent.get_tracking_no() - 1):
             return
         self.parent.swap_order(index, index + 1)
+        self.parent.scroll_frame((index) / self.parent.get_tracking_no())
         self.parent.refresh_elements()
-
-    def AddEntryPoint(self):
-        """Adds an certain type of entry form in a certain position
-
-        A direct copy of edu_module_editor's AddEntryPoint, it just works
-        for the edu_module_editor instead
-
-        God bless this spaghetti code"""
-        index_to_add = self.master.get_index_instance()
-        to_add = self.chosen_para_type.get()
-
-        entry_form = self.main_editor.widget_factory(to_add)
-
-        entry_form.focus()
-        self.parent.add_element_specific(index_to_add, entry_form)
-        self.parent.refresh_elements()
-        self.parent.scroll_frame(index_to_add / self.parent.get_tracking_no())
-
-if __name__ == "__main__":
-    from ...ui_app import App
-    a = App()
-
-    test = EntryAdder(a.main_frame, a, 35, 700)
-    test.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
-
-    a.main_frame.grid(row=0, column=0)
-    a.mainloop()
+        self.master.focus()
